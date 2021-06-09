b8454ad97898d5b795ec73ab336db74c12394b60
theSwana <sven.obranic.smb@gmail.com>
2021-06-09 22:22:55 +0200

    Daljnji rad

--- Projekt/Pokusaj 1/E-Sneaker Project/ESneakerTest/ESneakerTest.cpp
+++ Projekt/Pokusaj 1/E-Sneaker Project/ESneakerTest/ESneakerTest.cpp
@@ -12,47 +12,48 @@ int main()
 {
     //*******Opcenito*******
     string DaNe;
-    int n;
-    int* ddpolje = new int[100 * 100]; 
-    int s;
 
     //*******Korisnik*******
     int izbor;
-    string* IDD = new  string[200];
-    string* password = new  string[200];
-    string* IMEIPREZIME = new string[200];
-    double* stanje = new double[200];
+    string* IDD = new  string[2];
+    string* password = new  string[2];
+    string* IMEIPREZIME = new string[2];
+    long double* stanje = new long double[2];
+    float Velicina;
     int brk = 0;
 
     //*******Tenisice*******
-    string* Naziv = new string[20];
-    float Ve[100][100];
-    string* boja = new string[100];
-    double* cijena = new double[20];
+    string* Naziv = new string[200];
+    double* cijena = new double[200];;
     int brT = 0;
+    int brojtenisica;
 
     //*******Kupnja*********
     int brKU = 0;
-    string* ImeTenisica = new string[10000];
-    float* Velicina = new float[10000];
+    string* ImeTenisica = new string[200];
+    string* boja = new string[200];
+    int rednibroj = 1;
+    int ez;
+
 
     while (1)
     {
     jump:
         system("cls");
-        cout << "*   DOBRODOSLI U E-SNEAKER  *" << endl;
-        cout << "\n" << endl;
-        cout << "\n" << endl;
-        cout << "IZBORNIK: " << endl;
-        cout << "\n" << endl;
-        cout << "1. Sign up\n";
-        cout << "2. Informacije o racunu\n";
-        cout << "3. Dodavanje tenisica\n";
-        cout << "4. Pregled dostupnih tenisica\n";
-        cout << "5. Kosarica\n";
-        cout << "6. Izlaz iz programa\n";
+        cout << "*   DOBRODOSLI U E-SNEAKER  *";
+        cout << "\n" ;
+        cout << "IZBORNIK: ";
         cout << "\n";
-        cout << "Unesite izbor: ";
+        cout << "   1. Sign up\n";
+        cout << "   2. Informacije o racunu\n";
+        cout << "   3. Dodavanje tenisica\n";
+        cout << "   4. Pregled dostupnih tenisica\n";
+        cout << "   5. Kupovina\n";
+        cout << "   6. Kosarica\n";
+        cout << "   7. Izlaz iz programa\n";
+        cout << "\n";
+        cout << "\n";
+        cout << "               >>Unesite izbor: ";
         cin >> izbor;
         system("cls");
 
@@ -68,6 +69,8 @@ int main()
                 cout << "* Unesite ime i prezime: ";
                 cin.ignore();
                 getline(cin, IMEIPREZIME[brk]);
+                cout << "* Unesite vasu velicinu noge: ";
+                cin >> Velicina;
                 cout << "* Unesite stanje na racunu: ";
                 cin >> stanje[brk];
                 system("pause");
@@ -91,9 +94,6 @@ int main()
                     system("pause");
                     goto jump;
                 }
-
-
-
             }
 
         }
@@ -104,6 +104,7 @@ int main()
 
                 cout << "=> Username: " << IDD[i] << endl;
                 cout << "=> Ime i prezime: " << IMEIPREZIME[i] << endl;
+                cout << "=> Preferirana velicina cipela: " << Velicina<<" (EU)" << endl;
                 cout << "=> Stanje na racunu: " << stanje[i] << " kn" << endl;
                 cout << "\n";
             }
@@ -114,17 +115,6 @@ int main()
             cout << "Unestite naziv tenisica: ";
             cin.ignore();
             getline(cin, Naziv[brT]);            
-            cout << "Odaberite broj dostupnih velicina: ";
-            cin >> n;
-            int i = 0;
-            int x = 0;
-            while (i < n && x == brT) {
-                cin>> ddpolje[i&x];
-                i++;
-            }
-            cout << "Unesite dostupne boje: ";
-            cin.ignore();
-            getline(cin, boja[brT]);
             cout << "Unesite cijenu tenisica: ";
             cin >> cijena[brT];
             brT++;
@@ -137,36 +127,64 @@ int main()
             for (int i = 0; i < brT; i++) {
                 cout << "\n";
                 cout << "Naziv: "<<Naziv[i]<<endl;
-                cout << "Unesite broj tenisica za koje zelite vidjeti dostupne velicine velicine: ";
-                cin >> s;
-                int x = 0;
-                int p = 0;
-                while (x == s - 1 && p <= sizeof(ddpolje[p*x])) {
-                    cout << i;
-                    cout << ddpolje[p&x];
-                    p++;
-                }
-                cout << "Boje: "<<boja[i]<<endl;
                 cout << "Cijena: "<<cijena[i]<<endl;
                 cout << "\n";
-                system("pause");
             }
+            system("pause");
+        }
+        else if (izbor == 5)
+        {
+            cout << "Izbor tenisica: ";
+            for (int i = 0; i < brT; i++) {
+                cout << "\n";
+                cout << rednibroj<<"."<<endl;
+                cout << "Naziv: " << Naziv[i] << endl;
+                cout << "Cijena: " << cijena[i] << endl;
+                cout << "\n";
+                rednibroj++;
+            }
+            cout << "Unesite koje biste tenisice voljeli kupiti: ";
+            cin >> ez;
+            for (int i = 0; i == (ez - 1); i++) {
+                cout << "\n";
+                cout << "Odabrali ste tenisice [" << Naziv[i] << "]"<<"\n";
+                cout << "Unesite zeljenu boju tenisica: \n";
+                cin.ignore();
+                getline(cin, boja[brKU]);
+                cout << "Vasa velicina tenisica je: " << Velicina<<"\n";
+                cout << "Cijena tenisica je: " << cijena[i] << " kn"<<"\n";
+                ImeTenisica[brKU] = Naziv[i];
+                brKU++;
+            }
+            
             system("pause");
         }
         else if (izbor == 6)
         {
-            break;
+            cout << "Vasa kosarica: "<<endl;
+            for (int i = 0; i < brKU; i++) {
+                cout << "\n";
+                cout << "Tenisice: "<<ImeTenisica[i]<<endl;
+                cout << "Boja: "<<boja[i]<<endl;
+                cout << "Velicina: "<<Velicina<<endl;
+                cout << "Cijena: "<<cijena[i]<<endl;
+            }
+            cout << "\n: ";
+            
+            system("pause");
         }
         else if (izbor == 7)
         {
-            for (int i = 0; i < brT; i++)
-            {
-                cout << "Naziv: " << Naziv[i];
-                cout << "Velicine: " << Ve[i];
-                cout << "Boje: " << boja[i];
-                cout << "Cijena: " << cijena[i];
-            }
+            break;
+        }
+        else if (izbor == 69)
+        {
+            cout << "   ! OVO NIJE STRANICA TAKVE VRSTE !"<<endl;
+            cout << "        ! SRAM TE I STID BILO !"<<endl;
+            cout << "   ! SAD SE VRATI NA POCETNI MENU !"<<endl;
             system("pause");
+            goto jump;
+            
         }
         else
         {
